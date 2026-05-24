52f777ed281be12c1379922785012ff9c7757aba
Siddharth Nahar <77606988+sidd68-hub@users.noreply.github.com>
2026-05-24 21:15:31 +0530

    Migrated portfolio from Flutter to Next.js

--- .next/dev/static/chunks/app_page_tsx_12uwdpq._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_three_build_three_core_0qefhn6.js","static/chunks/node_modules_three_build_three_module_024z3_h.js","static/chunks/node_modules_three_build_three_module_12n2-ho.js","static/chunks/node_modules_@react-three_fiber_dist_12xg~9y._.js","static/chunks/node_modules_motion-dom_dist_es_0n_wqqr._.js","static/chunks/node_modules_framer-motion_dist_es_10zhgje._.js","static/chunks/node_modules_@rive-app_canvas_rive_0v3-ddg.js","static/chunks/node_modules_0xropz0._.js","static/chunks/app_page_tsx_0~3a212._.js"],
-    source: "dynamic"
-});
